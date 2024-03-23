5b9b86d0c66aead022824800facf690c943bd445
huangyp <harry@geekworm.com>
2024-03-23 20:45:23 +0800

    update

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Uninstall x729-fan.service:
+sudo systemctl stop x729-fan
+sudo systemctl disable x729-fan
+
+file_path="/lib/systemd/system/x729-fan.service"
+if [ -f "$file_path" ]; then    
+    sudo rm -f "$file_path"    
+fi
+
+file_path="/usr/local/bin/x729-fan.sh"
+if [ -f "$file_path" ]; then    
+    sudo rm -f "$file_path"    
+fi
+
+sudo sed -i '/dtoverlay=pwm-2chan/d' /boot/firmware/config.txt
+
 # Uninstall x729 installation script
 sudo systemctl stop x729-pwr
 sudo systemctl disable x729-pwr
