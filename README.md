83b6e9d7dfa10685f5eb559152b6503252bbe03f
TB-X1C\huangyp <harry@geekworm.com>
2024-03-25 23:56:34 +0800

    update

--- README.md
+++ README.md
@@ -2,3 +2,34 @@
 User Guide: https://wiki.geekworm.com/X729-script
 
 Email: support@geekworm.com
+
+# Update
+Use gpiod instead of obsolete interface, and suuports ubuntu 23.04 also
+
+## Software shutdown service:
+
+PWM_CHIP=0
+
+BUTTON=26
+
+
+> /usr/local/bin/xSoft.sh 0 20
+
+## Power management service
+PWMCHIP=0
+
+SHUTDOWN=5
+
+BOOT=12
+
+>/usr/local/bin/xPWR.sh 0 5 12
+
+## If working with Raspberry Pi 5 hardware, the following changes need to be made after cloning the file
+```
+sed -i 's/xSoft.sh 0 26/xSoft.sh 4 26/g' install-sss.sh
+
+sed -i 's/xPWR.sh 0 5 12/xPWR.sh 4 5 12/g' x729-pwr.service
+```
+> echo "alias x728off='sudo /usr/local/bin/xSoft.sh 0 26'" >>   ~/.bashrc
+
+> echo "alias x728off='sudo /usr/local/bin/xSoft.sh 0 16'" >>   ~/.bashrc
--- uninstall.sh
+++ uninstall.sh
@@ -3,50 +3,37 @@
 # Uninstall x729-fan.service:
 sudo systemctl stop x729-fan
 sudo systemctl disable x729-fan
-
 file_path="/lib/systemd/system/x729-fan.service"
-if [ -f "$file_path" ]; then    
-    sudo rm -f "$file_path"    
+if [ -f "$file_path" ]; then
+    sudo rm -f "$file_path"
 fi
 
 file_path="/usr/local/bin/x729-fan.sh"
-if [ -f "$file_path" ]; then    
-    sudo rm -f "$file_path"    
+if [ -f "$file_path" ]; then
+    sudo rm -f "$file_path"
 fi
-
-sudo sed -i '/dtoverlay=pwm-2chan/d' /boot/firmware/config.txt
 
 # Uninstall x729 installation script
 sudo systemctl stop x729-pwr
 sudo systemctl disable x729-pwr
 file_path="/lib/systemd/system/x729-pwr.service"
-if [ -f "$file_path" ]; then    
-    sudo rm -f "$file_path"    
+if [ -f "$file_path" ]; then
+    sudo rm -f "$file_path"
 fi
 
-file_path="/usr/local/bin/x729-pwr.sh"
-if [ -f "$file_path" ]; then    
-    sudo rm -f "$file_path"    
-fi
-
-file_path="/usr/local/bin/xgpio_pwr"
-
-if [ -f "$file_path" ]; then    
-    sudo rm -f "$file_path"    
+file_path="/usr/local/bin/xPWR.sh"
+if [ -f "$file_path" ]; then
+    sudo rm -f "$file_path"
 fi
 
 # Remove the xoff allias on .bashrc file
 sudo sed -i '/x729off/d' ~/.bashrc
 source ~/.bashrc
 
-file_path="/usr/local/bin/x729-softsd.sh"
-if [ -f "$file_path" ]; then    
-    sudo rm -f "$file_path"    
+file_path="/usr/local/bin/xSoft.sh"
+if [ -f "$file_path" ]; then
+    sudo rm -f "$file_path"
 fi
 
-file_path="/usr/local/bin/xgpio_soft"
-if [ -f "$file_path" ]; then    
-    sudo rm -f "$file_path"    
-fi
-
-sudo sed -i '/dtoverlay=pwm-2chan,pin2=13,func2=4/d' /boot/firmware/config.txt
+# Remove the configuratoin of config.txt
+sudo sed -i '/dtoverlay=pwm-2chan/d' /boot/firmware/config.txt
