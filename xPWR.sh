692cbc0e544e3b6dbb4737c7b914daa1888217ff
hmxf <gouych13@icloud.com>
2024-12-21 01:44:34 +0800

    Removed odd spaces

--- xPWR.sh
+++ xPWR.sh
@@ -1,15 +1,15 @@
-#!/bin/bash  
+#!/bin/bash
 
 #  Use gpiod instead of obsolete interface, and suuports ubuntu 23.04 also
 
-# 确保有足够的参数被传入  
-if [ "$#" -ne 3 ]; then  
-  echo "Usage: $0 <gpio_chip> <shutdown_pin> <boot_pin>"  
-  exit 1  
-fi  
+# 确保有足够的参数被传入
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <gpio_chip> <shutdown_pin> <boot_pin>"
+  exit 1
+fi
 
-GPIOCHIP=$1    
-SHUTDOWN=$2  
+GPIOCHIP=$1
+SHUTDOWN=$2
 BOOT=$3
 
 # 检查传入的参数是否为整数
@@ -26,31 +26,31 @@ if ! [[ $BOOT =~ $re ]] ; then
    echo "error: button_pin is not a number" >&2; exit 1
 fi
 
-REBOOTPULSEMINIMUM=200  
-REBOOTPULSEMAXIMUM=600  
-  
-# 初始化BOOT引脚为1  
-gpioset $GPIOCHIP $BOOT=1  
-  
-while [ 1 ]; do  
-  shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)  
-  if [ $shutdownSignal -eq 0 ]; then  
-    sleep 0.2  
-  else  
-    pulseStart=$(date +%s%N | cut -b1-13)  
-    while [ $shutdownSignal -eq 1 ]; do  
-      sleep 0.02  
-      if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then  
-        echo "Your device is shutting down on pin $SHUTDOWN, halting Rpi ..."  
-        sudo poweroff  
-        exit  
-      fi  
-      shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)  
-    done  
-    if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then  
-      echo "Your device is rebooting on pin $SHUTDOWN, recycling Rpi ..."  
-      sudo reboot  
-      exit  
-    fi  
-  fi  
+REBOOTPULSEMINIMUM=200
+REBOOTPULSEMAXIMUM=600
+
+# 初始化BOOT引脚为1
+gpioset $GPIOCHIP $BOOT=1
+
+while [ 1 ]; do
+  shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)
+  if [ $shutdownSignal -eq 0 ]; then
+    sleep 0.2
+  else
+    pulseStart=$(date +%s%N | cut -b1-13)
+    while [ $shutdownSignal -eq 1 ]; do
+      sleep 0.02
+      if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
+        echo "Your device is shutting down on pin $SHUTDOWN, halting Rpi ..."
+        sudo poweroff
+        exit
+      fi
+      shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)
+    done
+    if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then
+      echo "Your device is rebooting on pin $SHUTDOWN, recycling Rpi ..."
+      sudo reboot
+      exit
+    fi
+  fi
 done
