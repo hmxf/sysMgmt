38266cb02b2353bf1d995e377c78a1f4b7b090f2
hmxf <gouych13@icloud.com>
2024-12-21 14:37:03 +0800

    Updated echo command with tee

--- scripts/1.base_config.sh
+++ scripts/1.base_config.sh
@@ -17,27 +17,27 @@ echo "Add system configurations:"
 SYSTEM_CONFIG=$(cat /boot/firmware/config.txt | grep dtoverlay)
 
 if ! grep -q "dtoverlay=pwm-2chan,pin2=13,func2=4" "$SYSTEM_CONFIG"; then
-    sudo echo "dtoverlay=pwm-2chan,pin2=13,func2=4" >> /boot/firmware/config.txt
+    echo "dtoverlay=pwm-2chan,pin2=13,func2=4" | sudo tee -a /boot/firmware/config.txt
     echo "'dtoverlay=pwm-2chan,pin2=13,func2=4' has been added in the config file."
 else
     echo "'dtoverlay=pwm-2chan,pin2=13,func2=4' already exists in the config file."
 fi
 
 if ! grep -q "dtoverlay=i2c-rtc,ds1307" "$SYSTEM_CONFIG"; then
-    sudo echo "dtoverlay=i2c-rtc,ds1307" >> /boot/firmware/config.txt
+    echo "dtoverlay=i2c-rtc,ds1307" | sudo tee -a /boot/firmware/config.txt
     echo "'dtoverlay=i2c-rtc,ds1307' has been added in the config file."
 else
     echo "'dtoverlay=i2c-rtc,ds1307' already exists in the config file."
 fi
 
 if ! grep -q "usb_max_current_enable=1" "$SYSTEM_CONFIG"; then
-    sudo echo "usb_max_current_enable=1" >> /boot/firmware/config.txt
+    echo "usb_max_current_enable=1" | sudo tee -a /boot/firmware/config.txt
     echo "'usb_max_current_enable=1' has been added in the config file."
 else
     echo "'usb_max_current_enable=1' already exists in the config file."
 fi
 
-PSU_MAX_CURRENT=$(sudo rpi-eeprom-config)
+PSU_MAX_CURRENT=$(sudo rpi-eeprom-config | grep PSU_MAX)
 
 if ! grep -q "PSU_MAX_CURRENT=5000" "$PSU_MAX_CURRENT"; then
     echo "PSU_MAX_CURRENT=5000 did not exist in the config file. Please add it manually."
