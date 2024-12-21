717167394a6372b82520a2a2195a8f6f65b28803
hmxf <gouych13@icloud.com>
2024-12-21 21:17:35 +0800

    Updated OTBR install script

--- scripts/3.install_otbr.sh
+++ scripts/3.install_otbr.sh
@@ -1,6 +1,17 @@
 # Install and configure OTBR software stack
+
+# Fetch OTBR source code
 cd ~
 git clone https://github.com/openthread/ot-br-posix.git --depth 1
+
+# Configure NPM to install packages globally
+sudo apt-get install --no-install-recommends -y nodejs npm
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.bashrc && source ~/.bashrc
+npm list -g --depth=0
+
+# Prepare and install OTBR
 cd ot-br-posix
 WEB_GUI=1 ./script/bootstrap
 INFRA_IF_NAME=end0 WEB_GUI=1 ./script/setup
