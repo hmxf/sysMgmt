50eb84a6ad6f70840457c9cbd710c975b1e763f1
hmxf <gouych13@icloud.com>
2024-12-23 09:49:19 +0800

    Added another method for OTBR installation, choose as need

---
+++ scripts/install_otbr_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install and configure OTBR software stack
+cd ~
+git clone https://github.com/openthread/ot-br-posix.git --depth 1
+cd ot-br-posix
+./script/bootstrap
+INFRA_IF_NAME=eth0 ./script/setup
